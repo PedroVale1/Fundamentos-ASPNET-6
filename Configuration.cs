8dabb9058dbc6da9b942e90565830e2575403cd0
PedroVale1 <162756423+PedroVale1@users.noreply.github.com>
2025-01-18 20:33:25 -0300

    Add files via upload

---
+++ Configuration.cs
@@ -0,0 +1,18 @@
+﻿namespace Blog
+{
+    public static class Configuration
+    {
+        public static string JwtKey = "ZmVkYW3ZDg4NjNiNdH";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api_ASDQWQWKewaERTFEed/z0ae2Nwe/unQwes=";
+        public static StmpConfiguration Smtp = new();
+
+        public class StmpConfiguration
+        {
+            public string Host { get; set; }
+            public int Port { get; set; } = 25;
+            public string UserName { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
